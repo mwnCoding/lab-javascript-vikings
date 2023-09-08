83e59c37b0c1074b2d2ca251c41a449296a3b0e4
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-08 15:13:28 +0200

    Iteration 2 done

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,30 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
+
+const viking = new Viking("Gundar", 10, 15);
+console.log(viking);
 
 // Saxon
 class Saxon {}
